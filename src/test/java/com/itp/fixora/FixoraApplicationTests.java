9275ac80c076c4318975fee5f48094d4adb278a8
Nayan <nayandahare02@gmail.com>
2026-04-01 20:03:01 +0530

    Initial Commit

---
+++ src/test/java/com/itp/fixora/FixoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itp.fixora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
